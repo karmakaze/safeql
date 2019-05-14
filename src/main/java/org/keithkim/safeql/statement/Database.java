c97dd6cd5c644746d4e06621660538636af73b1b
Ankush Chaudhary <ankushchaudhary348@gmail.com>
2019-05-14 22:33:08 +0530

    Add in-memory db for running query

--- src/main/java/org/keithkim/safeql/statement/Database.java
+++ src/main/java/org/keithkim/safeql/statement/Database.java
@@ -5,16 +5,22 @@ import lombok.EqualsAndHashCode;
 import org.jdbi.v3.core.Jdbi;
 import org.jdbi.v3.sqlobject.SqlObjectPlugin;
 
+import java.util.Optional;
+
 @EqualsAndHashCode
 public class Database {
     public final HikariDataSource ds;
     public final Jdbi jdbi;
 
     public Database(String jdbcUrl, String username, String password) {
+        this(jdbcUrl, username, password, null);
+    }
+    public Database(String jdbcUrl, String username, String password, String driverClassName) {
         ds = new HikariDataSource();
         ds.setJdbcUrl(jdbcUrl);
         ds.setUsername(username);
         ds.setPassword(password);
+        Optional.ofNullable(driverClassName).ifPresent(ds::setDriverClassName);
         jdbi = Jdbi.create(ds);
         jdbi.installPlugin(new SqlObjectPlugin());
     }
